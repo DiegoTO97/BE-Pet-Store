c439ae61c8a70b1421759fb7b08e92bfb3f6d1da
DiegoTO97 <ditoalpacaman@gmail.com>
2022-10-25 18:23:02 -0400

    first commint

---
+++ BE-CRUDMascotas/Models/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Models
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Mascota> Mascotas { get; set; }
+
+        internal object FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
